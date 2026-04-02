0edc54aeadb3c7d9e2498872efcb8f6d140492e0
Murilo <murilojeronimoferreiranunes10@gmail.com>
2026-04-02 11:43:02 -0300

    criando a pagina Home

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,8 +1,32 @@
+import '../css/estilo.css'
+import '../css/global.css'
+import Imagem from '../assets/imgs/academia.png'
+
 const Home = () => {
   return (
-    <div>
+    <div className='home'>
+      <section className='hero'>
       <h2>Bem-vindo à Nossa Academia</h2>
       <p>Descubra o melhor em treinamento e saúde!</p>
+      <button className='btn'>Começar agora</button>
+      </section>
+      <section className='imagemsection'>
+      <img src={Imagem} alt="Academia inteligente" className='img'/>
+      </section>
+      <section className='cards'>
+        <div className='card'>
+          <h2>Treinos Inteligentes</h2>
+          <p>IA que adapta seu treino automaticamente</p>
+        </div>
+        <div className='card'>
+          <h2>Acompanhamento</h2>
+          <p>Veja sua evolução em tempo real</p>
+        </div>
+        <div className='card'>
+          <h2>Alta Performace</h2>
+          <p>Equipamentos modernos e tecnologia avançada</p>
+        </div>
+      </section>
     </div>
   )
 }
