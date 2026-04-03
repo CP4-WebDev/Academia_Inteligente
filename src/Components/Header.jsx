f323654d2e59920375132a08397c69535fa19600
Maria_Luiza <lukochnoff@gmail.com>
2026-04-03 13:00:52 -0300

    refactor:  melhorias no código do Header, add logo e icons

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,16 +1,25 @@
 import { Link } from "react-router-dom";
+import "../css/header.css";
 
 const Header = () => {
   return (
     <header className="header">
-      <nav className="nav">
-        <Link to="/">Home</Link>
-        <Link to="/planos">Planos</Link>
-        <Link to="/imc">IMC</Link>
-        <Link to="/sobre">Sobre</Link>
-        <Link to="/contato">Contato</Link>
-        
-      </nav>
+      <div className="header-inner">
+
+        <Link to="/" className="header-logo">
+          <i className="bi bi-lightning-charge-fill"></i>
+          <span>Academia <em>Inteligente</em></span>
+        </Link>
+
+        <nav className="header-nav">
+          <Link to="/"><i className="bi bi-house"></i> Home</Link>
+          <Link to="/planos"><i className="bi bi-card-list"></i> Planos</Link>
+          <Link to="/imc"><i className="bi bi-activity"></i> IMC</Link>
+          <Link to="/sobre"><i className="bi bi-info-circle"></i> Sobre</Link>
+          <Link to="/contato"><i className="bi bi-envelope"></i> Contato</Link>
+        </nav>
+
+      </div>
     </header>
   );
 };
